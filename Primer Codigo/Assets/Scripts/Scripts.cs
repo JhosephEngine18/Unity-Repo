21e312ca71213a3412df041a5552c4ce4dcdc8c0
Jhosephek <josesielo1805@gmail.com>
2024-09-14 20:30:51 -0600

    Unity Stuff

---
+++ Primer Codigo/Assets/Scripts/Scripts.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Algo paso");
+        Debug.Log("Hola");
+        Debug.LogWarning("Cuidado");
+        Debug.LogError("Noooo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
